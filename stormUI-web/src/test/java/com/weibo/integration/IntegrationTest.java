9ab6111923673bdc0bd81439ab77da7ee5aded78
shixi_jianchuan1 <shixi_jianchuan1@staff.weibo.com>
2016-05-31 15:18:05 +0800

    add integration test

---
+++ stormUI-web/src/test/java/com/weibo/integration/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.weibo.integration;
+
+import org.junit.Test;
+
+/**
+ * Created by meng on 16-5-31.
+ */
+public class IntegrationTest {
+    @Test
+    public void testApp()
+    {
+        System.out.println("IntegrationTest.testApp");
+    }
+
+    @Test
+    public void testApp2()
+    {
+        System.out.println("IntegrationTest.testApp2");
+    }
+}
